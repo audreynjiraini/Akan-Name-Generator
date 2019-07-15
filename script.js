dc68805bebb8da5fdf4f36e9a9ae42c0485aaac8
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-07-15 13:58:38 +0300

    make some changes

--- script.js
+++ script.js
@@ -60,31 +60,44 @@ function checkDayOfTheWeek(){
 
     if(gender=="male" && calculate==0){
         document.write("Your were born on "+days[0]+" and your Akan name is "+maleNames[0]);
-    } else if (gender=="female"&& calculate==0){
+    } 
+    else if (gender=="female"&& calculate==0){
         document.write("You were born on "+days[0]+" and your Akan name is "+femaleNames[0]);
-    }else if (gender=="male"&& calculate==1){
+    }
+    else if (gender=="male"&& calculate==1){
         document.write("Your were born on "+days[1]+" and your Akan name is "+maleNames[1]);
-    } else if(gender=="female"&& calculate==1){
+    }
+    else if(gender=="female"&& calculate==1){
         document.write("Your were born on "+days[1]+" and your Akan name is "+femaleNames[1]);
-    } else if (gender=="male" && calculate==2){
+    }
+    else if (gender=="male" && calculate==2){
         document.write("You were born on "+days[2]+" and your Akan name is "+maleNames[2]);
-    } else if (gender=="female" && calculate==2){
+    }
+    else if (gender=="female" && calculate==2){
         document.write("You were born on "+days[2]+" and your Akan name is "+femaleNames[2]);
-    } else if (gender=="male" && calculate==3){
+    }
+    else if (gender=="male" && calculate==3){
         document.write("You were born on "+days[3]+" and your Akan name is "+maleNames[3]);
-    } else if (gender=="female" && calculate==3){
+    }
+    else if (gender=="female" && calculate==3){
         document.write("You were born on "+days[3]+" and your Akan name is "+femaleNames[3]);
-    } else if (gender=="male" && calculate==4){
+    }
+    else if (gender=="male" && calculate==4){
         document.write("You were born on "+days[4]+" and your Akan name is "+maleNames[4]);
-    } else if (gender=="female" && calculate==4){
+    }
+    else if (gender=="female" && calculate==4){
         document.write("You were born on "+days[4]+" and your Akan name is "+femaleNames[4]);
-    } else if (gender=="male" && calculate==5){
+    }
+    else if (gender=="male" && calculate==5){
         document.write("You were born on "+days[5]+" and your Akan name is "+maleNames[5]);
-    } else if (gender=="female" && calculate==5){
+    }
+    else if (gender=="female" && calculate==5){
         document.write("You were born on "+days[5]+" and your Akan name is "+femaleNames[5]);
-    } else if (gender=="male" && calculate==6){
+    }
+    else if (gender=="male" && calculate==6){
         document.write("You were born on "+days[6]+" and your Akan name is "+maleNames[6]);
-    } else {
+    }
+    else {
         document.write("You were born on "+days[6]+" and your Akan name is "+femaleNames[6]);
     }
 }
